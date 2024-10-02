7b45d15831bc827da0f7bdd0878e8699d6ab3d79
Alek Vernelen <alek.vernelen@student.ucll.be>
2024-10-03 00:23:53 +0200

    done

--- 01-arithmetic/26-assignment-internet-costs/student.py
+++ 01-arithmetic/26-assignment-internet-costs/student.py
@@ -1,1 +1,9 @@
 # write your code here
+from math import floor
+def internet_costs(duration_in_seconds, cost_per_block):
+    x = duration_in_seconds//360
+    rest = duration_in_seconds%360
+    if rest > 0:
+        x += 1
+    total_cost = x*cost_per_block
+    return total_cost
