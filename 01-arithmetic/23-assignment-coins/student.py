6640d48f6944b031a77ac5e1d2f25c3d8e630b75
Alek Vernelen <alek.vernelen@student.ucll.be>
2024-10-02 23:27:07 +0200

    coins done

--- 01-arithmetic/23-assignment-coins/student.py
+++ 01-arithmetic/23-assignment-coins/student.py
@@ -1,1 +1,8 @@
 # write your code here
+def coins(amount):
+    fives = amount//5
+    twos = (amount-(fives*5))//2
+    ones = ((amount-(fives*5))-(twos*2))//1
+    a = (fives,twos,ones)
+    return sum(a)
+
