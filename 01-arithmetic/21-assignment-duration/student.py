79debf97455d38eb41f91bbc56d92ff36c11087d
Alek Vernelen <alek.vernelen@student.ucll.be>
2024-10-02 22:26:23 +0200

    done

--- 01-arithmetic/21-assignment-duration/student.py
+++ 01-arithmetic/21-assignment-duration/student.py
@@ -1,1 +1,16 @@
 # write your code here
+def hours(duration):
+    h_in_duration = duration//3600
+    return h_in_duration
+
+def minutes(duration):
+    h = hours(duration)
+    minutes_in_leftover = (duration-(h*3600))//60
+    return minutes_in_leftover
+
+def seconds(duration):
+    h = hours(duration)
+    m = minutes(duration)
+    seconds_in_leftover = duration-(h*3600)-(m*60)
+    return seconds_in_leftover
+
