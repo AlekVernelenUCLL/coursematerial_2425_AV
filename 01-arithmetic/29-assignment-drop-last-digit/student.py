37c4f5e73e0dfb85db63f3c81be7737f52dd8b2d
Alek Vernelen <alek.vernelen@student.ucll.be>
2024-10-03 14:43:33 +0200

    done

--- 01-arithmetic/29-assignment-drop-last-digit/student.py
+++ 01-arithmetic/29-assignment-drop-last-digit/student.py
@@ -1,9 +1,2 @@
-# write your code here
 def drop_last_digit(n):
-    a = str(n)
-    n_digits = len(a)
-    index = n_digits-1
-    removed_last_digit = a.replace(a[index],'')
-    return int(removed_last_digit)
-
-print(drop_last_digit())
+    return (n//10)
