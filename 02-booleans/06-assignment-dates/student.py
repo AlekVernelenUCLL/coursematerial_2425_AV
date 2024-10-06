c37b471f00b1b73e64a1c083900377f0e9b65a21
Alek Vernelen <alek.vernelen@student.ucll.be>
2024-10-06 14:09:28 +0200

    not finished

--- 02-booleans/06-assignment-dates/student.py
+++ 02-booleans/06-assignment-dates/student.py
@@ -1,1 +1,9 @@
-# write your code here
+def is_valid_month(month):
+    return 1<=month<=12
+
+def is_leap_year(year):
+    yes = year%4==0 or (year%100==0 and year//400==0)
+    nott = year%100==0 and year%400!=0
+    return yes or nott
+
+def has_30_days(month):
