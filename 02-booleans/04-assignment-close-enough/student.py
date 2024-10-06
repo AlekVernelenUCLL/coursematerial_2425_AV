c404f0e2b92e667ae147cc8180c532d420a22248
Alek Vernelen <alek.vernelen@student.ucll.be>
2024-10-06 13:45:17 +0200

    stuck

--- 02-booleans/04-assignment-close-enough/student.py
+++ 02-booleans/04-assignment-close-enough/student.py
@@ -1,2 +1,16 @@
 def close_enough(x, y):
-    
+    return -0.1<=x-y<=0.1 or -0.1<=y-x<=0.1
+
+a=close_enough(0, 0)
+
+b=close_enough(0, 0.05)
+
+c=close_enough(0, 0.1)
+
+d=close_enough(0, -0.1)
+
+e=close_enough(0, 0.2)
+
+f=close_enough(18.5, 18.6)
+
+print(a,b,c,d,e,f)
