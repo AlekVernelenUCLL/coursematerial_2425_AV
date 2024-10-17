b7c96231fc6ddcd0de8256a76edc8ad7ef2f09a8
Alek Vernelen <alek.vernelen@student.ucll.be>
2024-10-17 13:14:40 +0200

    done

--- 05-strings/06-assignment-interactive-greet/student.py
+++ 05-strings/06-assignment-interactive-greet/student.py
@@ -1,3 +1,6 @@
 def interactive_greet():
-    name = input("What is your name? ")
-    print(f"Hello, {name}!")
+    name = input("What is your name? ") 
+    print(greet(name))
+
+def greet(name):
+    return f"Hello, {name}!"
