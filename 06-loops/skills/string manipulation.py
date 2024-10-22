b6b5f5a1ebf3b50baff3d10d3fe73152d7b69e05
Alek Vernelen <alek.vernelen@student.ucll.be>
2024-10-22 17:16:36 +0200

    done

--- 06-loops/skills/string manipulation.py
+++ 06-loops/skills/string manipulation.py
@@ -1,13 +1,17 @@
 def remove_double_spaces(text):
+    s = ""
+    count = 0
     for char in text:
         if char == " ":
             count += 1
+        elif count >= 1:
+            s += " "+char
+            count = 0
         else:
-            if count > 1:
-                
-            else:
-                s += char
+            s += char
+    return f"'{s}'"
 
 
-a = "dit    is    een    voorbeeldtekst"
+
+a = "   Deze tekst dient  als      voorbeeld  voor deze   oefening  "
 print(remove_double_spaces(a))
