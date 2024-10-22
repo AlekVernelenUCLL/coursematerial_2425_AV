17ff74948ad31a588cb3bea69fbb1f1374c45b82
Alek Vernelen <alek.vernelen@student.ucll.be>
2024-10-22 18:38:29 +0200

    done

--- 06-loops/08-assignment-rpg/student.py
+++ 06-loops/08-assignment-rpg/student.py
@@ -1,13 +1,22 @@
 def rpg2(n_sides,goal):
+    numb = 0
+    count = 0
     for i in range(1,n_sides+1):
         for j in range(1,n_sides+1):
             sum = i+j
+            numb += 1
             if sum >= goal:
                 count += 1
+    return count/numb*100
 
-
-# def rpg3(n_sides,goal):
-#     for i in range(1,n_sides+1):
-#         rpg2(n_sides,goal)
-
-
+def rpg3(n_sides,goal):
+    numb = 0
+    count = 0
+    for i in range(1,n_sides+1):
+        for j in range(1,n_sides+1):
+            for k in range(1,n_sides+1):
+                sum = i+j+k
+                numb += 1
+                if sum >= goal:
+                    count += 1
+    return count/numb*100
