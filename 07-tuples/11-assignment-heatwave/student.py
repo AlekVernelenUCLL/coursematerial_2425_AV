fd0134b1bc8903088473a3c8da1940ece850ff8f
Alek Vernelen <alek.vernelen@student.ucll.be>
2024-11-07 15:29:47 +0100

    not finished

--- 07-tuples/11-assignment-heatwave/student.py
+++ 07-tuples/11-assignment-heatwave/student.py
@@ -1,1 +1,11 @@
-# write your code here
+def heatwave(temperatures):
+    count25 = 0
+    count30 = 0
+    for temp in temperatures:
+        if 25 >= temp < 30:
+            count25 += 1
+        elif temp > 30:
+            count30 += 1
+        else:
+            count25 = 0
+            count30 = 0
