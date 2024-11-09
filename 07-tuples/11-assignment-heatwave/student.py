700701ecc587711d62537989fd7035267cab8b61
Alek Vernelen <alek.vernelen@student.ucll.be>
2024-11-09 11:51:47 +0100

    done

--- 07-tuples/11-assignment-heatwave/student.py
+++ 07-tuples/11-assignment-heatwave/student.py
@@ -2,10 +2,17 @@ def heatwave(temperatures):
     count25 = 0
     count30 = 0
     for temp in temperatures:
-        if 25 >= temp < 30:
-            count25 += 1
-        elif temp > 30:
-            count30 += 1
+        if count25 >= 5 and count30 >=3:
+            return True
         else:
-            count25 = 0
-            count30 = 0
+            if 25 <= temp < 30:
+                count25 += 1
+            elif temp >= 30:
+                count25 += 1
+                count30 += 1
+            else:
+                count25 = 0
+                count30 = 0
+    if count25 >= 5 and count30 >=3:
+        return True
+    return False
--- 07-tuples/14-assignment-versioning/student.py
+++ 07-tuples/14-assignment-versioning/student.py
@@ -1,1 +1,22 @@
-# write your code here
+def increase_version(version, breaking_change, new_features):
+    major, minor, patch = version
+    if breaking_change:
+        major += 1
+        minor = 0
+        patch = 0
+    elif new_features:
+        minor += 1
+        patch = 0
+    else:
+        patch += 1
+    return (major,minor,patch)
+
+# print(increase_version((0,0,0),breaking_chance=False,new_features=True))
+
+def is_more_recent(v1, v2):
+    return v1 > v2
+
+# print(is_more_recent((0,0,1),(0,1,0)))
+
+def is_older(v1, v2):
+    return v1 < v2
