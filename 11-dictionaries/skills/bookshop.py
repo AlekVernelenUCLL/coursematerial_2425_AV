36930945bba6fbc60e8cb109136494088466e707
Alek Vernelen <alek.vernelen@student.ucll.be>
2024-11-20 03:35:52 +0100

    not finished

---
+++ 11-dictionaries/skills/bookshop.py
@@ -0,0 +1,15 @@
+books = {
+    "1984": ["George Orwell", "Fiction", 5],
+    "Sapiens": ["Yuval Noah Harari", "Non-Fiction", 10],
+    "To Kill a Mockingbird": ["Harper Lee", "Fiction", 8],
+    "A Brief History of Time": ["Stephen Hawking", "Science", 7],
+    "The Catcher in the Rye": ["J.D. Salinger", "Fiction", 6],
+    "Becoming": ["Michelle Obama", "Biography", 12],
+    "The Great Gatsby": ["F. Scott Fitzgerald", "Fiction", 4],
+    "Homo Deus": ["Yuval Noah Harari", "Non-Fiction", 9],
+    "The Subtle Art of Not Giving a F*ck": ["Mark Manson", "Self-Help", 15],
+    "Educated": ["Tara Westover", "Biography", 10]
+}
+
+def add_books(books, title, author, genre, stock):
+    
