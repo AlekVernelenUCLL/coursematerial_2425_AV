2a6ec708c7ac6a8c2d069ff1331582c639014cf8
Alek Vernelen <alek.vernelen@student.ucll.be>
2024-12-02 12:41:00 +0100

    not finished

--- 12-objects/15-assignment-tweet/student.py
+++ 12-objects/15-assignment-tweet/student.py
@@ -1,17 +1,22 @@
 class Tweet:
     def __init__(self, message, max_length):
-        self.message = message
         self.max_length = max_length
+        self.message = message
 
     @property
     def message(self):
         return self.__message
     
-    @message.setter
+    @property
+    def max_length(self):
+        return self.__max_length
+    
+    message.setter
     def message(self, message):
-        self.__message = message
+        pass
 
-    def valid(self, message, max_length):
-        max_length_minimum = max_length >= 1
-        if max_length_minimum:
-            return len(message) <= max_length
+    max_length.setter
+    def max_length(self, max_length):
+        if max_length >= 1 and len(self.message) <= max_length:
+            self.__max_length = max_length
+        if len(self.message) <=
